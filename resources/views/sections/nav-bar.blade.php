ab54013cb8bd9e08b3c9f9a260571279bd54e664
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-02-29 13:23:02 +0100

    fix: Header

--- resources/views/sections/nav-bar.blade.php
+++ resources/views/sections/nav-bar.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid p-0">
     <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
-        <a href="index.html" class="navbar-brand ml-lg-3">
-            <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Faster</h1>
+        <a href="{{route('home')}}" class="navbar-brand ml-lg-3">
+            <h1 class="m-0 display-5 text-uppercase text-primary"><i class="fa fa-truck mr-2"></i>Elevate</h1>
         </a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
